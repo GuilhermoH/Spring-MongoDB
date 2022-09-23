babbdbbef4fa6ed5e49e6c6048311b9d33a40764
GuilhermoH <guilhermoh@gmail.com>
2022-09-23 10:43:02 -0300

    findById and Exceptions Added

---
+++ workshop/src/main/java/com/herter/workshop/exceptions/StandartError.java
@@ -0,0 +1,24 @@
+package com.herter.workshop.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class StandartError implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long timestamp;
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
+
+
+}
